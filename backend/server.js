64fa0be3cde1dce5a678312a9095d6ef790dd5de
Gunjan Garg <payalgarg18082828@gmail.com>
2025-03-02 01:25:42 +0530

    04:35:07 - 04:47:35 Created Backend Server and File Structure

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+//App Config
+const app = express()
+const port = process.env.PORT || 4000
+
+//Middlewares
+app.use(express.json())
+app.use(cors())
+
+//API Endpoints
+app.get ('/',(req,res)=>{
+    res.send("API WORKING")
+})
+
+app.listen(port, ()=> console.log("Server Startes", port))
