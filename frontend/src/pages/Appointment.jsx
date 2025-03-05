ff1a513af6cd9488418b9060d66b4b01b79c530b
Gunjan Garg <payalgarg18082828@gmail.com>
2025-03-05 18:23:40 +0530

    Updated Errors

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -15,7 +15,7 @@ const Appointment = () => {
     const [slotTime, setSlotTime] = useState('');
 
     const fetchDocInfo = async () => {
-        const docInfo = doctors.find(doc => doc.id === docId);
+        const docInfo = doctors.find(doc => doc._id === docId);
         setDocInfo(docInfo)
     }
 
@@ -78,7 +78,7 @@ const Appointment = () => {
             {/* ----------- Doctor Details ------------ */}
             <div className='flex flex-col sm:flex-row gap-4'>
                 <div>
-                    <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.imageUrl} alt={docInfo.name} /> 
+                    <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt={docInfo.name} /> 
                 </div>
                 <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
                     {/* ----------------- Doc Info : name, degree, experience ---------- */}
